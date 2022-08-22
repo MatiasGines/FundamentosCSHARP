1780d9ee24076cdf58a6257186c2f8664d0b4e6d
MatiasGines <41976674+MatiasGines@users.noreply.github.com>
2022-08-22 11:39:39 -0300

    Agregar archivos de proyecto.

---
+++ FundamentosCSHARP/Models/Vino.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosCSHARP.Models
+{
+    class Vino : Bebida, IBebidaAlcoholica
+    {
+
+        public int Alcohol { get; set; }
+
+        public void MaxRecomendado()
+        {
+            Console.WriteLine("El maximo permitido son 3 copas");
+        }
+        public Vino(int Cantidad, string Nombre = "Vino")
+            : base(Nombre, Cantidad)
+        {
+
+        }
+
+    }
+}
---
+++ FundamentosCSHARP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FundamentosCSHARP.Models;
+
+namespace FundamentosCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CervezaBD cervezaBD = new CervezaBD();
+
+            //insertamos nuevos datos
+            /*{
+                Cerveza cerveza = new Cerveza(15, "Red Ipa");
+                cerveza.Marca = "Lupita";
+                cerveza.Alcohol = 5;
+                cervezaBD.Add(cerveza);
+
+            }*/
+            {
+
+                cervezaBD.Delete(3);
+            }
+                //obtenemos las cervezas de la BD
+                var cervezas = cervezaBD.Get();
+
+            foreach (var cerveza in cervezas)
+            {
+                Console.WriteLine(cerveza.Nombre);
+            }
+
+        }
+    }
+
+}
